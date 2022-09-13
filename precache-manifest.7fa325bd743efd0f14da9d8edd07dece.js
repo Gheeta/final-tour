a5ea536a968a9ccee79f072ab6a4c3a3e8412802
Gheeta <101050138+Gheeta@users.noreply.github.com>
2022-09-13 02:34:40 +0200

    Updates

---
+++ precache-manifest.7fa325bd743efd0f14da9d8edd07dece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd4b51af78f27800bfb17c1fca3df0b",
+    "url": "/final-tour/index.html"
+  },
+  {
+    "revision": "5d328c1a76370179b035",
+    "url": "/final-tour/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/final-tour/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/final-tour/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d328c1a76370179b035",
+    "url": "/final-tour/static/js/main.554acd0a.chunk.js"
+  },
+  {
+    "revision": "4a655079b4f231fae32f",
+    "url": "/final-tour/static/js/runtime-main.29f02694.js"
+  }
+]);
